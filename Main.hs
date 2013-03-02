deae5db22050f8df12cd12110d3693ac42ca35b1
Konstantin Savelyev <ksavelev@gmail.com>
2013-03-02 20:45:56 +0100

    using getLine

--- Main.hs
+++ Main.hs
@@ -3,8 +3,5 @@ import System.Environment
 
 main :: IO ()
 main = do
-    args <- getArgs
-    let n1 = read $ args !! 0
-    let n2 = read $ args !! 1
-    let sm = n1 + n2
-    putStrLn ("Hello, " ++ (show sm))
+    name <- getLine
+    putStrLn ("Hello, " ++ name)
